7da9b37f827f76daaa60e2408c68aaa7c774c47e
Lorcan <lnick2440@gmail.com>
2026-02-21 20:36:28 +0000

    improved integrator docstrings

--- integrators.py
+++ integrators.py
@@ -24,23 +24,30 @@ class IntegratorType(Enum):
         return self.name
 
 
-# TODO: consider adding RK4 for Stratonovich SDEs, based on Frankignoul and Hasselmann (1976)
-# source: https://github.com/bekaiser/SDE
-
-
-# integrators for ODEs (use_ode_mode = True)
+################################################
+## Integrators for ODEs (use_ode_mode = True) ##
+################################################
 
 def integrate_rk4(plant, t_start: float, t_stop: float, dt: float) -> tuple[np.ndarray, np.ndarray]:
     """
-    Integrate plant dynamics using the fixed-step Runge-Kutta 4th order method (RK4)
-    over [t_start, t_stop].
+    Solves the ODE: dx/dt = A x + B u + w_proc, where w_proc is a constant process noise term
+    sampled at the start of the integration interval. This is not mathematically equivalent to
+    solving the true SDE, since the noise is not treated as a stochastic diffusion term.
+
+    This method uses the Runge-Kutta 4th order method (RK4) to get an approximate solution to the ODE, with time step dt.
+
+    ### Arguments
+    - `plant`: the plant to integrate
+    - `t_start`: start time of the integration interval. Shape: scalar
+    - `t_stop`: stop time of the integration interval. Shape: scalar
+    - `dt`: time step for the output time points. Shape: scalar
+
+    ### Returns
+    - `tuple[np.ndarray, np.ndarray]`: arrays of time points and state trajectory over the integration interval 
+    (including both endpoints).
 
     NOTE: RK4 assumes a smooth RHS. The process noise (modelled as Gaussian noise) is not 
-    differentiable, so RK4 is not strictly appropriate for stochastic differential equations (SDEs). 
-    However, we can still use RK4 as a numerical integration method for SDEs by treating the 
-    noise as constant across each `dt_int` (additive drift, rather than diffusion). This has the 
-    effect of scaling the noise by a factor of `sqrt(dt_int)`, and results are therefore not 
-    comparable between RK4 and the other methods, which solve the true SDE.
+    differentiable, so RK4 is not appropriate for use in stochastic differential equation (SDE) mode.
     """
 
     # create array [t_start, t_start + dt, t_start + 2 dt, ..., t_stop]
@@ -55,6 +62,8 @@ def integrate_rk4(plant, t_start: float, t_stop: float, dt: float) -> tuple[np.n
 
     Bu = plant.B @ plant.u  # shape (dims, 1)
 
+    # in ODE mode, we treat process noise as a constant drift term across the integration interval
+    # sample the process noise once at the start of the interval from N(0, Q) and use it throughout the interval
     w_proc_i = plant.sample_process_noise(n=1)  # shape (dims, 1)
 
     for i in range(num_steps - 1):
@@ -76,57 +85,9 @@ def integrate_rk4(plant, t_start: float, t_stop: float, dt: float) -> tuple[np.n
 
     return t_span, x_span
 
-# integrators for SDEs (use_ode_mode = False)
-
-def integrate_euler_maruyama(plant, t_start: float, t_stop: float, dt: float) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Integrate plant dynamics with the Euler-Maruyama method over [t_start, t_stop].
-
-    Solves the Itô SDE: dx = (A x + B u) dt + G dW_t, where W_t is a Wiener process 
-    (E[dW_t @ dW_t.T] = I dt), so that the covariance matrix of the diffusion term (process noise)
-    is Q = G @ G.T.
-
-    NOTE: if needed, we can compute G using the Cholesky decomposition of Q, 
-    since Q is positive semidefinite. However, drawing samples from N(0, Q) is more efficient
-    (and mathemtically equivalent) than drawing samples from N(0, I) and multiplying by G.
-    """
-
-    # create array [t_start, t_start + dt, t_start + 2 dt, ..., t_stop]
-    t_span = get_t_span(t_start, t_stop, dt)  # shape (num_steps,)
-    num_steps = t_span.shape[0]
-
-    # set empty arrays
-    x_span = np.zeros((plant.dims, num_steps))  # shape (dims, num_steps)
-
-    # set initial state
-    x_span[:, 0] = plant.x.reshape(plant.dims,)
-
-    Bu = plant.B @ plant.u  # shape (dims, 1)
-
-    w_proc = plant.sample_process_noise(n=num_steps - 1)  # shape (dims, num_steps - 1)
-
-    for i in range(num_steps - 1):
-
-        # get t_i
-        t_i = t_span[i]
-        dt_i = t_span[i + 1] - t_i
-        x_i = x_span[:, i].reshape(plant.dims, 1)  # shape (dims, 1)
-
-        w_proc_i = w_proc[:, i].reshape(plant.dims, 1)
-        sqrt_dt = np.sqrt(dt_i)
-        # Euler-Maruyama update:
-        # x_{i+1} = x_i + (A x_i + B u) dt + w_i sqrt(dt), where w_i ~ N(0, Q)
-        x_span[:, i + 1] = x_span[:, i] + (plant.A @ x_i + Bu)[:, 0] * dt_i + w_proc_i[:, 0] * sqrt_dt
-
-    return t_span, x_span
-
-
 def integrate_analytic_ode(plant, t_start: float, t_stop: float, dt: float) \
         -> tuple[np.ndarray, np.ndarray]:
     """
-    Integrate plant dynamics with an analytic linear-system solution, treating noise as a 
-    constant drift term across the integration interval (`use_ode_mode = True`).
-
     Solves the ODE: dx/dt = A x + B u + w_proc, where w_proc is a constant process noise term
     sampled at the start of the integration interval. This is not mathematically equivalent to
     solving the true SDE, since the noise is not treated as a stochastic diffusion term.
@@ -142,7 +103,8 @@ def integrate_analytic_ode(plant, t_start: float, t_stop: float, dt: float) \
     - `dt`: time step for the output time points. Shape: scalar
 
     ### Returns
-    - `tuple[np.ndarray, np.ndarray]`: arrays of time points and state trajectory over the integration interval (including both endpoints).
+    - `tuple[np.ndarray, np.ndarray]`: arrays of time points and state trajectory over the integration interval 
+    (including both endpoints).
     """
     t_span = plant.t_span_0 + t_start
     I = np.eye(plant.dims)
@@ -158,15 +120,77 @@ def integrate_analytic_ode(plant, t_start: float, t_stop: float, dt: float) \
     x_span = (Phi @ plant.x + Phi_plus @ Bu_plus_w)[:, :, 0].T  # shape (dims, num_steps)
     return t_span, x_span
 
+
+#################################################
+## Integrators for SDEs (use_ode_mode = False) ##
+#################################################
+
+# TODO: consider adding RK4 for Stratonovich SDEs, based on Frankignoul and Hasselmann (1976)
+# source: https://github.com/bekaiser/SDE
+
+def integrate_euler_maruyama(plant, t_start: float, t_stop: float, dt: float) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Solves the Itô SDE: dx = (A x + B u) dt + d_eta_t, where eta_t is a scaled Wiener process 
+    given by eta_t = G @ W_t, where W_t is a standard Wiener process. These processes satisfy the Itô isometry
+    (E[dW_t @ dW_t.T] = I dt and E[d_eta_t @ d_eta_t.T] = Q dt), so that the covariance matrix of the 
+    diffusion term (process noise) is Q = G @ G.T.
+
+    This method uses the Euler-Maruyama method to get an approximate solution to the SDE, with time step dt.
+
+    ### Arguments
+    - `plant`: the plant to integrate
+    - `t_start`: start time of the integration interval. Shape: scalar
+    - `t_stop`: stop time of the integration interval. Shape: scalar
+    - `dt`: time step for the output time points. Shape: scalar
+
+    ### Returns
+    - `tuple[np.ndarray, np.ndarray]`: arrays of time points and state trajectory over the integration interval 
+    (including both endpoints).
+
+    NOTE: if needed, we can compute G using the Cholesky decomposition of Q, since Q is positive semidefinite. 
+    However, drawing samples from N(0, Q) is more efficient (and mathemtically equivalent) than drawing 
+    samples from N(0, I) and multiplying by G.
+    """
+
+    # create array [t_start, t_start + dt, t_start + 2 dt, ..., t_stop]
+    t_span = get_t_span(t_start, t_stop, dt)  # shape (num_steps,)
+    num_steps = t_span.shape[0]
+
+    # set empty arrays
+    x_span = np.zeros((plant.dims, num_steps))  # shape (dims, num_steps)
+
+    # set initial state
+    x_span[:, 0] = plant.x.reshape(plant.dims,)
+
+    Bu = plant.B @ plant.u  # shape (dims, 1)
+
+    # NOTE: draw from N(0, Q) for each time step to get diffusion innovations
+    # this gets scaled by sqrt(dt) in the update loop to get the correct covariance for the diffusion term
+    eta_proc = plant.sample_process_noise(n=num_steps - 1)  # shape (dims, num_steps - 1)
+
+    for i in range(num_steps - 1):
+
+        # get t_i
+        t_i = t_span[i]
+        dt_i = t_span[i + 1] - t_i
+        x_i = x_span[:, i].reshape(plant.dims, 1)  # shape (dims, 1)
+
+        eta_proc_i = eta_proc[:, i].reshape(plant.dims, 1)
+        sqrt_dt = np.sqrt(dt_i)
+        # Euler-Maruyama update:
+        # x_{i+1} = x_i + (A x_i + B u) dt + eta_i sqrt(dt), where eta_i ~ N(0, Q)
+        x_span[:, i + 1] = x_span[:, i] + (plant.A @ x_i + Bu)[:, 0] * dt_i + eta_proc_i[:, 0] * sqrt_dt
+
+    return t_span, x_span
+
 def integrate_analytic_sde(plant, t_start: float, t_stop: float, dt: float) \
         -> tuple[np.ndarray, np.ndarray]:
     
     """
-    Integrate plant dynamics with an analytic linear-system solution, treating noise as a
-    stochastic diffusion term across the integration interval (`use_ode_mode = False`).
-    Solves the Itô SDE: dx = (A x + B u) dt + G dW_t, where W_t is a Wiener process
-    (E[dW_t @ dW_t.T] = I dt), so that the covariance matrix of the diffusion term (process 
-    noise) is Q = G @ G.T.
+    Solves the Itô SDE: dx = (A x + B u) dt + d_eta_t, where eta_t is a scaled Wiener process 
+    given by eta_t = G @ W_t, where W_t is a standard Wiener process. These processes satisfy the Itô isometry
+    (E[dW_t @ dW_t.T] = I dt and E[d_eta_t @ d_eta_t.T] = Q dt), so that the covariance matrix of the 
+    diffusion term (process noise) is Q = G @ G.T.
 
     This method uses the Van Loan method to compute the exact discrete-time process noise
     covariance matrix Q_d over the integration interval, and samples noise from N(0, Q_d) to
